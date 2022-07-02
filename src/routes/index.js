965e263130153e1169bea2e707526ebb345556ff
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-02 16:48:48 +0100

    Add login route to be mounted

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const register = require('./register.js');
+const login = require('./login.js');
 
 module.exports = app => {
-  app.use('/register', register)
+  app.use('/register', register);
+  app.use('/login', login);
 };
