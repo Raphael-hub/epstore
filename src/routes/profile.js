0e00732ecb02a13a177c2db1f4c64c374a45a437
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-02 23:12:01 +0100

    Add profile and update profile routes

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,40 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const db = require('../db/helpers.js');
 
-router.get('/', (req, res, next) => {
+const isLoggedIn = (req, res, next) => {
   if (!req.user) {
     return res.status(401).json({ error: 'Not logged in' });
+  } else {
+    return next();
   }
-  res.status(200).json(req.user);
+};
+
+router.get('/', isLoggedIn, (req, res, next) => {
+  res.status(200).json({
+    info: `Logged in as user ${req.user.username}`
+  });
+});
+
+router.put('/update', isLoggedIn, async (req, res, next) => {
+  try {
+    // Fill from current user details and update any changes from body
+    let changes = req.body;
+    if (changes.password && changes.password !== req.user.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(changes.password, salt);
+      changes.password = hashedPassword;
+    }
+    const updated = { ...req.user, ...changes };
+    const user = await db.updateUserById(req.user.id, updated);
+    if (!user) {
+      return res.status(400).json({ error: 'Unable to update user' });
+    }
+    return res.status(200).json(user);
+    //return res.status(200).json({ info: 'Successfully update user: ${req.user.username}' });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = router;
