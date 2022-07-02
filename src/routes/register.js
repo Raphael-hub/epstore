13512903a1c10f6d3d85e3d8e8b08ad8535fa5b0
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-02 14:51:08 +0100

    Add bcypt to hash password before adding to database

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,17 +1,21 @@
 const Router = require('express-promise-router');
-const router = new Router();
-
+const bcrypt = require('bcrypt');
 const db = require('../db/helpers.js');
+
+const router = new Router();
 
 router.post('/', async (req, res, next) => {
   const { username, password, email, name, address } = req.body;
-  const user = { username, password, email, name, address };
   try {
     if (await db.getUserByUsername(username)) {
       res.status(403).json({ info: 'Username already exists' });
     } else if (await db.getUserByEmail(email)) {
       res.status(403).json({ info: 'Email already in use' });
     } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = { username, password: hashedPassword, email, name, address };
+
       const result = await db.createUser(user);
       res.status(201).json(result);
     }
