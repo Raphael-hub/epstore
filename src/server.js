21c8470ff231d25625c13f046d8a71e83142a507
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-04 17:54:45 +0100

    Rename app.js to server.js

---
+++ src/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config({ path: '../.env' });
+
+const mountRoutes = require('./routes/index.js');
+const { pool } = require('./db/index.js');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const store = new session.MemoryStore();
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 900000, secure: false },
+    rolling: true,
+    saveUninitialized: false,
+    resave: false,
+    store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mountRoutes(app);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  res.status(status).json({ error: err.message });
+});
+
+process.on('SIGINT', () => {
+  pool.end(() => {
+    console.log('[db] pool has ended');
+  });
+  console.log('[server] shutting down');
+  process.exit();
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`[server] listening on port ${port}`)
+})
