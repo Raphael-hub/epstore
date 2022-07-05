5afe4a4e0f24641cf6278aa99ceefbb04022e4c5
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-05 15:00:42 +0100

    Add helper functions for CRUD actions with products table

--- src/db/helpers.js
+++ src/db/helpers.js
@@ -34,9 +34,9 @@ const getUserByEmail = async (email) => {
 };
 
 const createUser = async (user) => {
+  const { username, password, email, name, address } = user;
+  const date = new Date();
   try {
-    const { username, password, email, name, address } = user;
-    const date = new Date();
     const { rows } = await query(
       'INSERT INTO users (username, password, email, name, address, created_at) \
       VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
@@ -85,6 +85,129 @@ const deleteUserById = async (id) => {
   }
 };
 
+const tableHasColumn = async (column) => {
+  try {
+    const { rows } = await query(
+      'SELECT column_name FROM information_schema.columns \
+      WHERE table_name = \'products\''
+    );
+    if (!rows.includes(column)) {
+      return false;
+    }
+    return true;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getProductById = async (id, column = 'listed_at', sort = 'desc') => {
+  try {
+    if (!await tableHasColumn(column)) {
+      throw new Error('Unknown column to sort by');
+    }
+    if (!['asc', 'desc'].includes(sort.toLowerCase())) {
+      throw new Error('Unknown direction to sort in');
+    }
+    const { rows } = await query(
+      'SELECT * FROM products WHERE id = $1 \
+      ORDER BY $2 $3',
+      [id, column, sort.toUpperCase()]
+    );
+    return rows[0] || null;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getProductsByName = async (name, column = 'listed_at', sort = 'desc') => {
+  try {
+    if (!await tableHasColumn(column)) {
+      throw new Error('Unknown column to sort by');
+    }
+    if (!['asc', 'desc'].includes(sort.toLowerCase())) {
+      throw new Error('Unknown direction to sort in');
+    }
+    const { rows } = await query(
+      'SELECT * FROM products WHERE name = $1 \
+      ORDER BY $2 $3',
+      [name, column, sort.toUpperCase()]
+    );
+    if (rows.length === 1) return rows[0];
+    return rows || null;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getProducts = async (column = 'listed_at', sort = 'desc') => {
+  try {
+    if (!await tableHasColumn(column)) {
+      throw new Error('Unknown column to sort by');
+    }
+    if (!['asc', 'desc'].includes(sort.toLowerCase())) {
+      throw new Error('Unknown direction to sort in');
+    }
+    const { rows } = await query(
+      'SELECT * FROM products ORDER BY $1 $2',
+      [column, sort.toUpperCase()]
+    );
+    if (rows.length === 1) return rows[0];
+    return rows || null;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const createProduct = async (product) => {
+  if (!product.currency) {
+    product.currency = 'gbp';
+  }
+  const { user_id, name, description, price, currency, stock } = product;
+  const date = new Date();
+  try {
+    const { rows } = await query(
+      'INSERT INTO products \
+      (user_id, name, description, price, currency, stock, listed_at) \
+      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+      [user_id, name, description, price, currency, stock, date.toISOString()]
+    );
+    return rows[0] || null;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const updateProductById = async (id, updates) => {
+  const { name, description, price, currency, stock } = updates;
+  try {
+    const result = await getProductById(id);
+    if (!result) {
+      throw new Error('Product not found');
+    }
+    const { rows } = await query(
+      'UPDATE products \
+      SET (name, description, price, currency, stock) = ($1, $2, $3, $4, $5) \
+      WHERE id = $6 RETURNING *',
+      [name, description, price, currency, stock, id]
+    );
+    return rows[0] || null;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const deleteProductById = async (id) => {
+  try {
+    const { rows } = await query(
+      'DELETE FROM products WHERE id = $1 RETURNING *',
+      [id]
+    );
+    return rows[0] || null;
+  } catch (err) {
+    throw err;
+  }
+};
+
 module.exports = {
   users: {
     getUserById,
@@ -92,7 +215,14 @@ module.exports = {
     getUserByEmail,
     createUser,
     updateUserById,
-    deleteUserById
+    deleteUserById,
   },
-  products: {},
+  products: {
+    getProductById,
+    getProductsByName,
+    getProducts,
+    createProduct,
+    updateProductById,
+    deleteProductById,
+  },
 };
