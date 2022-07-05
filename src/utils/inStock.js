6ccd8e6c3de916bb0581ebb99c76256ca0378b16
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-05 17:23:42 +0100

    Add stock level checker functions

---
+++ src/utils/inStock.js
@@ -0,0 +1,25 @@
+const { products } = require('../db/helpers.js');
+
+const isInStock = async (product_id) => {
+  try {
+    const product = await products.getProductById(product_id);
+    if (product.stock === 0) return false;
+    return true;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getProductStock = async (product_id) => {
+  try {
+    const product = await products.getProductById(product_id);
+    return product.stock;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  isInStock,
+  getProductStock
+};
