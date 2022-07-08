f91561c56bd514353f5d3f8d2888f55302bcd0b7
Raphael Prinz <raphaelprinz78@gmail.com>
2022-07-08 09:07:19 +0100

    still tinkering

--- src/routes/product.js
+++ src/routes/product.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res, next) => {
     return res.status(200).json({ products: listings });
   } catch (err) {
     return res.json({error: something up})
-    return next(err);
+    
   }
 });
 
