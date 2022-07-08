d1b7f984c5b28244c39072474ec39123378e8aef
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-08 12:49:35 +0100

    Update logging info

--- src/db/index.js
+++ src/db/index.js
@@ -20,7 +20,7 @@ module.exports = {
       const start = Date.now();
       const res = await pool.query(text, params);
       const duration = Date.now() - start;
-      console.log('[db] executed query', { text, duration, rows: res.rowCount });
+      console.log('[db] executed query', { text, params, duration, rows: res.rowCount });
       return res;
     } catch (err) {
       throw err;
@@ -39,17 +39,18 @@ module.exports = {
       console.error('[db] last executed query', { query: client.lastQuery });
     }, 5000);
 
-    client.query = (...args) => {
-      client.lastQuery = args;
-      console.log('[db] executed query', { query: client.lastQuery });
-      return query.apply(client, args);
+    client.query = async (text, params) => {
+      if (!params) params = [];
+      client.lastQuery = { text, params };
+      console.log('[db] executed query', { text, params });
+      return query.apply(client, [text, params]);
     };
 
     client.release = () => {
       clearTimeout(timeout);
       client.query = query;
       client.release = release;
-      console.log('[db] releasing a client');
+      console.log('[db] releasing client');
       return release.apply(client);
     };
 
