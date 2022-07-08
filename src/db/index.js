b2ef1c2ffcab0138926432777f9df9d61237be00
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-08 11:11:17 +0100

    Change logging to always on and add client checkout function

--- src/db/index.js
+++ src/db/index.js
@@ -5,15 +5,13 @@ const pool = new Pool({
   connectionString: DATABASE_URL
 });
 
-if (NODE_ENV === 'development' || null) {
-  pool.on('connect', () => {
-    console.log('[db] client connected to database');
-  });
+pool.on('connect', () => {
+  console.log('[db] client connected to database');
+});
 
-  pool.on('remove', () => {
-    console.log('[db] client removed from pool');
-  });
-}
+pool.on('remove', () => {
+  console.log('[db] client removed from pool');
+});
 
 module.exports = {
   pool,
@@ -22,12 +20,39 @@ module.exports = {
       const start = Date.now();
       const res = await pool.query(text, params);
       const duration = Date.now() - start;
-      if (NODE_ENV === 'development') {
-        console.log('[db] executed query', { text, params, duration, rows: res.rowCount });
-      }
+      console.log('[db] executed query', { text, duration, rows: res.rowCount });
       return res;
     } catch (err) {
       throw err;
     }
+  },
+  async getClient() {
+    const client = await pool.connect();
+    if (!client) return null;
+    console.log('[db] a client has been checked out');
+
+    const query = client.query;
+    const release = client.release;
+
+    const timeout = setTimeout(() => {
+      console.error('[db] a client has been checked out for 5 seconds');
+      console.error('[db] last executed query', { query: client.lastQuery });
+    }, 5000);
+
+    client.query = (...args) => {
+      client.lastQuery = args;
+      console.log('[db] executed query', { query: client.lastQuery });
+      return query.apply(client, args);
+    };
+
+    client.release = () => {
+      clearTimeout(timeout);
+      client.query = query;
+      client.release = release;
+      console.log('[db] releasing a client');
+      return release.apply(client);
+    };
+
+    return client;
   }
 };
